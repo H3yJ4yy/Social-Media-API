6401d0e46103a46da94cf4e09595b8163afd50fa
H3yJ4yy <143395836+H3yJ4yy@users.noreply.github.com>
2024-02-03 22:14:07 -0600

    updated user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route("/friend/:userId").put(addFriend).delete(removeFriend);
+
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
